b2cc0f1447a49f43e8874e92bf3c9ae93333e448
tainfante <agatadul@gmail.com>
2018-01-21 19:34:57 +0100

    different data receiving concept

--- src/sample/Port.java
+++ src/sample/Port.java
@@ -14,8 +14,6 @@ public class Port {
 
     boolean Stop = false;
 
-    boolean StopAll = false;
-
     private static volatile Port PortInstance;
 
     private ArrayList<Byte> inputData;
@@ -44,7 +42,6 @@ public class Port {
             CommPortIdentifier portNumber = CommPortIdentifier.getPortIdentifier(portName);
             serialPort = (SerialPort) portNumber.open("Time to Digital Converter", 1000);
             serialPort.setSerialPortParams(baudRate, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
-            Controller.getInstance().log("Port has been opened \n");
             Stop = false;
 
             return true;
@@ -74,23 +71,13 @@ public class Port {
         }
     }
 
-    public void startReceiving(){
-        Thread receivingThread = new Thread(() ->
-        {
-            Controller.getInstance().log("Waiting to receive data \n");
-
-            while (!StopAll){
-                try{
-                    long time=readTime();
-                    Controller.getInstance().log("Estimated time is: "+time+ "\n");
-                }
-                catch (Exception e){
-                    e.printStackTrace();
-                }
-            }
-        });
-        receivingThread.setName("TimeThread");
-        receivingThread.start();
+    public void sendStart(){
+        try{
+            serialPort.getOutputStream().write(66);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private byte readOneByte(){
@@ -105,57 +92,69 @@ public class Port {
         }
         return oneByte;
     }
-    private long readTime() {
-        Stop=false;
+    public long readTime() {
         long time=0;
         long unsignedData=0;
         int numberOfBytes=0;
         byte data;
-        while (!Stop)
-        {
-            data = readOneByte();
-
-            if ( 0 > data )
+        while(time==0){
+            Stop=false;
+            while (!Stop)
             {
-                break;
-            }
-            if (data==1){
-                numberOfBytes++;
-                while(numberOfBytes!=0){
-                    switch(numberOfBytes){
-                        case 1:
-                            data=readOneByte();
-                            unsignedData=(data&0xFF);
-                            time=time|(unsignedData<<32);
-                            numberOfBytes++;
-                            break;
-                        case 2:
-                            data=readOneByte();
-                            unsignedData=(data&0xFF);
-                            time=time|(unsignedData<<24);
-                            numberOfBytes++;
-                            break;
-                        case 3:
-                            data=readOneByte();
-                            unsignedData=(data&0xFF);
-                            time=time|(unsignedData<<16);                            numberOfBytes++;
-                            break;
-                        case 4:
-                            data=readOneByte();
-                            unsignedData=(data&0xFF);
-                            time=time|(unsignedData<<8);                            numberOfBytes++;
-                            break;
-                        case 5:
-                            data=readOneByte();
-                            unsignedData=(data&0xFF);
-                            time=time|(unsignedData);
-                            numberOfBytes=0;
-                            Stop=true;
-                            break;
+                data = readOneByte();
+
+                if ( 0 > data )
+                {
+                    break;
+                }
+                if (data==1){
+                    numberOfBytes++;
+                    while(numberOfBytes!=0){
+                        switch(numberOfBytes){
+                            case 1:
+                                data=readOneByte();
+                                unsignedData=(data&0xFF);
+                                time=time|(unsignedData<<32);
+                                numberOfBytes++;
+                                break;
+                            case 2:
+                                data=readOneByte();
+                                unsignedData=(data&0xFF);
+                                time=time|(unsignedData<<24);
+                                numberOfBytes++;
+                                break;
+                            case 3:
+                                data=readOneByte();
+                                unsignedData=(data&0xFF);
+                                time=time|(unsignedData<<16);                            numberOfBytes++;
+                                break;
+                            case 4:
+                                data=readOneByte();
+                                unsignedData=(data&0xFF);
+                                time=time|(unsignedData<<8);                            numberOfBytes++;
+                                break;
+                            case 5:
+                                data=readOneByte();
+                                unsignedData=(data&0xFF);
+                                time=time|(unsignedData);
+                                numberOfBytes=0;
+                                Stop=true;
+                                break;
+                        }
                     }
                 }
             }
         }
+
         return time;
+    }
+
+    public void sendStop() {
+        try{
+            serialPort.getOutputStream().write(0);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
